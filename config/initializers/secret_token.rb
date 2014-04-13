3a3129fdc55a8deeb7915eaed6ae42334486a59d
C-A de Salaberry <cadesalaberry@yahoo.com>
2014-04-13 18:20:31 -0400

    Add: Homepage sketches.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spotlight::Application.config.secret_key_base = '5dcc6dbd71b93f4e454db0a0f2f81170badb3b4ff4dbd291e2669e7868dc6e0ac6f928514970eb58c2549c21170209ec29a9cec2206f22f1dd0840a341e74fe6'
